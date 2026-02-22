d931e3cfe353af4c09fad4c5215092499f174c2a
MatthewPhan <Matthewminhphan@gmail.com>
2026-02-21 17:10:13 -0800

    jobDescPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { InterviewProvider } from './context/InterviewContext';
 import SetupScreen from './screens/SetupScreen';
+import JobDescription from './screens/JobDescription';
 
 const InterviewScreen = lazy(() => import('./screens/InterviewScreen'));
 const FeedbackScreen = lazy(() => import('./screens/FeedbackScreen'));
@@ -27,6 +28,7 @@ export default function App() {
       <InterviewProvider>
         <Routes>
           <Route path="/" element={<div className="page-enter"><SetupScreen /></div>} />
+          <Route path="/job-description" element={<div className="page-enter"><JobDescription /></div>} />
           <Route path="/interview" element={
             <Suspense fallback={loadingFallback}>
               <div className="page-enter"><InterviewScreen /></div>
